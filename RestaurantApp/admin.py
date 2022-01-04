1db07401d2e414c384c542013f33d2cda7e4d9d8
Ubuntu <ubuntu@ip-172-31-32-186.ap-south-1.compute.internal>
2022-01-04 08:16:13 +0000

    Everything added for the Next instance

--- RestaurantApp/admin.py
+++ RestaurantApp/admin.py
@@ -5,7 +5,11 @@ from .models import Restaurant, Table, TempDelivery, Delivery
 class TableAdmin(admin.ModelAdmin):
     list_display = ['table_no', 'rest']
 
+class DeliveryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'restaurant', 'bot_name', 'bot_id', 'bot_color', 'table_no', 'time']
+    list_per_page =  500
+
 admin.site.register(Restaurant)
 admin.site.register(Table, TableAdmin)
 admin.site.register(TempDelivery)
-admin.site.register(Delivery)
+admin.site.register(Delivery, DeliveryAdmin)
